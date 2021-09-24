a0f022471e29bc7b6e46739262ea82c0022f9a2a
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-09-24 15:03:59 +0700

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -11,14 +11,14 @@ def list_drive(update, context):
     try:
         search = update.message.text.split(' ',maxsplit=1)[1]
         LOGGER.info(f"Searching: {search}")
-        reply = sendMessage('Searching..... Please wait!', context.bot, update)
+        reply = sendMessage('SedangMencari..... Sabar bos😅!', context.bot, update)
         gdrive = GoogleDriveHelper(None)
         msg, button = gdrive.drive_list(search)
 
         if button:
             editMessage(msg, reply, button)
         else:
-            editMessage('No result found', reply, button)
+            editMessage('Tidak Ditemukan apapun😌', reply, button)
 
     except IndexError:
         sendMessage('Send a search key along with command', context.bot, update)

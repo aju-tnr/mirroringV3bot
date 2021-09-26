59cb42b35b5580e8d09d9ca9900cf28a1972b6a9
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-09-26 12:17:34 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -99,15 +99,15 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>🗂️Filename:</b> <code>{download.name()}</code>"
+            msg += f"<b>🗂️NamaFile:</b> <code>{download.name()}</code>"
             msg += f"\n<b>⏰Status:</b> <i>{download.status()}</i>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
                 msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
                 if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                    msg += f"\n<b>💨Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    msg += f"\n<b>💨TerDownload:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 else:
-                    msg += f"\n<b>💨Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>⚡️Speed:</b> {download.speed()}\n<b>⏰ETA:</b> {download.eta()} "
+                    msg += f"\n<b>💨TerUpload:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                msg += f"\n<b>⚡️Kecepatan:</b> {download.speed()}\n<b>⏰ETA:</b> {download.eta()} "
                 # if hasattr(download, 'is_torrent'):
                 try:
                     msg += f"\n<b>🌾Seeders:</b> {download.aria_download().num_seeders}" \

e6d936e61d56ba847262d2b9b585bfd5ad90747b
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-09-26 20:58:49 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -38,15 +38,16 @@ def stats(update, context):
     stats = f'<b>╭──「⭕️ STATISTIK BOT ⭕️」</b>\n' \
             f'<b>│</b>\n' \
             f'<b>├ 🤖Bot Uptime:</b> {currentTime}\n' \
+            f'<b>│</b>\n' \
             f'<b>├ ⏰Start Time:</b> {current}\n' \
             f'<b>├ 💾Total Disk Space:</b> {total}\n' \
-            f'<b>├ 📀Used:</b> {used}  ' \
-            f'<b>├ 💿Free:</b> {free}\n\n' \
+            f'<b>├ 📀Used:</b> {used}\n' \
+            f'<b>├ 💿Free:</b> {free}\n' \
             f'<b>├ 🔼Upload:</b> {sent}\n' \
-            f'<b>├ 🔽Download:</b> {recv}\n\n' \
+            f'<b>├ 🔽Download:</b> {recv}\n' \
             f'<b>├ 🖥️CPU:</b> {cpuUsage}%\n' \
             f'<b>├ 🎮RAM:</b> {memory}%\n' \
-            f'<b>├ 💽DISK:</b> {disk}%' \
+            f'<b>├ 💽DISK:</b> {disk}%\n' \
             f'<b>│</b>\n' \
             f'<b>╰──「 🚸 @MirroringV3Bot 🚸 」</b>'
     update.effective_message.reply_photo(IMAGE_URL, stats, parse_mode=ParseMode.HTML)

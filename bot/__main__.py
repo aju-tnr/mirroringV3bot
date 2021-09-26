932720a5112af4379c6e9042f400b0994b705fee
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-09-26 20:14:50 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -35,16 +35,20 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>🤖Bot Uptime:</b> {currentTime}\n' \
-            f'<b>⏰Start Time:</b> {current}\n' \
-            f'<b>₪Total Disk Space:</b> {total}\n' \
-            f'<b>☞Used:</b> {used}  ' \
-            f'<b>☞Free:</b> {free}\n\n' \
-            f'😌Data Usage\n<b>▲Upload:</b> {sent}\n' \
-            f'<b>▼Download:</b> {recv}\n\n' \
-            f'<b>CPU:</b> {cpuUsage}%\n' \
-            f'<b>RAM:</b> {memory}%\n' \
-            f'<b>DISK:</b> {disk}%'
+    stats = f'<b>╭──「⭕️ STATISTIK BOT ⭕️」</b>\n' \
+            f'<b>│</b>\n' \
+            f'<b>├ 🤖Bot Uptime:</b> {currentTime}\n' \
+            f'<b>├ ⏰Start Time:</b> {current}\n' \
+            f'<b>├ 💾Total Disk Space:</b> {total}\n' \
+            f'<b>├ 📀Used:</b> {used}  ' \
+            f'<b>├ 💿Free:</b> {free}\n\n' \
+            f'<b>├ 🔼Upload:</b> {sent}\n' \
+            f'<b>├ 🔽Download:</b> {recv}\n\n' \
+            f'<b>├ 🖥️CPU:</b> {cpuUsage}%\n' \
+            f'<b>├ 🎮RAM:</b> {memory}%\n' \
+            f'<b>├ 💽DISK:</b> {disk}%' \
+            f'<b>│</b>\n' \
+            f'<b>╰──「 🚸 @MirroringV3Bot 🚸 」</b>'
     update.effective_message.reply_photo(IMAGE_URL, stats, parse_mode=ParseMode.HTML)
 
 

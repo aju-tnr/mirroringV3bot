9c87509abcb40664dfc61a75310896463e00e0b5
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-09-27 22:12:48 +0700

    Update stickers.py

--- bot/modules/stickers.py
+++ bot/modules/stickers.py
@@ -426,7 +426,7 @@ def stickhelp(update, context):
 • `/kang`*:* reply to a sticker to add it to your pack.
 • `/stickers`*:* Find stickers for given term on combot sticker catalogue
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/4fdda14844041a094df36.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
 
 STICKERID_HANDLER = CommandHandler("stickerid", stickerid)
 GETSTICKER_HANDLER = CommandHandler("getsticker", getsticker)

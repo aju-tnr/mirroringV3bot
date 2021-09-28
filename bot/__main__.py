d385a1c0b11b963b2ac23be15ee58778cc5d40be
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-09-28 17:08:52 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -18,7 +18,7 @@ from bot.helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper import button_build
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, search, delete, speedtest, usage, mediainfo, count, config, updates, anime, stickers, paste, gtranslator
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, search, delete, speedtest, usage, mediainfo, count, config, updates, anime, stickers
 
 now=datetime.now(pytz.timezone('Asia/Jakarta'))
 
@@ -155,10 +155,6 @@ def bot_help(update, context):
 
 /stickerhelp: Get help for stickers module.
 
-/paste: paste text to nekobin.
-
-/translatehelp: translate bahasa.
-
 /tshelp: Get help for Torrent search module.
 '''
 
@@ -218,10 +214,6 @@ def bot_help(update, context):
 /weebhelp: Get help for anime, manga and character module.
 
 /stickerhelp: Get help for stickers module.
-
-/paste: paste text to nekobin.
-
-/translatehelp: translate bahasa.
 
 /tshelp: Get help for Torrent search module.
 '''

2b395d98a45cd1eb51c6204409c86705d4faf6b4
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-09-28 14:16:16 +0700

    Update paste.py

--- bot/modules/paste.py
+++ bot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
 from bot import dispatcher
-from bot.modules.disable import CommandHandler
+from bot.modules.import CommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext
 
@@ -34,47 +34,11 @@ def paste(update: Update, context: CallbackContext):
         reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True,
     )
 
-import requests
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
-
-
-@run_async
-def paste(update: Update, context: CallbackContext):
-    args = context.args
-    message = update.effective_message
-
-    if message.reply_to_message:
-        data = message.reply_to_message.text
-
-    elif len(args) >= 1:
-        data = message.text.split(None, 1)[1]
-
-    else:
-        message.reply_text("What am I supposed to do with this?")
-        return
-
-    key = (
-        requests.post("https://nekobin.com/api/documents", json={"content": data})
-        .json()
-        .get("result")
-        .get("key")
-    )
-
-    url = f"https://nekobin.com/{key}"
-
-    reply_text = f"Nekofied to *Nekobin* : {url}"
-
-    message.reply_text(
-        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True,
-    )
 
 
 def pastehelp(update, context):
     help_string = '''
-• `/Nekopas`*:* Paste ke nekobin
+• `/Nekopas`*:* mempaste ke nekobin
 
 
 PASTE_HANDLER = CommandHandler("paste", paste)

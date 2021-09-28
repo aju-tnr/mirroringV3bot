fc2d9a915f17f4a1b34d8dcc32a3a9f60cf64bf9
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-09-28 14:39:12 +0700

    Update paste.py

--- bot/modules/paste.py
+++ bot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
 from bot import dispatcher
-from telegram import ParseMode, Update
+from telegram import ParseMode, Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext, CommandHandler
 
 

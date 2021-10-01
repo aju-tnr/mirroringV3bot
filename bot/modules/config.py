b1469f89beffeb3be25c0a1e780874f525c1897e
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-10-01 20:59:39 +0700

    Delete config.py

--- bot/modules/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    API_ID = "1224490" # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "692a022828c7470d13e517b856812afd" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "1964838308:AAEnXOkh9qhjyhjaPp4XGhAg1D4Ly_iW58s" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.

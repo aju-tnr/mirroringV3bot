2bbcb04aef3498570531a22d17900aa21585f47e
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-10-03 21:12:21 +0700

    Create paste.py

---
+++ bot/modules/paste.py
@@ -0,0 +1,40 @@
+import requests
+from bot import dispatcher
+from bot.modules.import CommandHandler
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
+
+
+@run_async
+def paste(update: Update, context: CallbackContext):
+    args = context.args
+    message = update.effective_message
+
+    if message.reply_to_message:
+        data = message.reply_to_message.text
+
+    elif len(args) >= 1:
+        data = message.text.split(None, 1)[1]
+
+    else:
+        message.reply_text("What am I supposed to do with this?")
+        return
+
+    key = (
+        requests.post("https://nekobin.com/api/documents", json={"content": data})
+        .json()
+        .get("result")
+        .get("key")
+    )
+
+    url = f"https://nekobin.com/{key}"
+
+    reply_text = f"Nekofied to *Nekobin* : {url}"
+
+    message.reply_text(
+        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True,
+    )
+
+
+PASTE_HANDLER = CommandHandler("paste", paste)
+dispatcher.add_handler(PASTE_HANDLER)

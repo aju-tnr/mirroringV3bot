d01705e299a7707f99eae66325a0d80d714e5d33
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-10-07 20:54:10 +0700

    Update text.py

--- bot/modules/text.py
+++ bot/modules/text.py
@@ -10,7 +10,10 @@ weebyfont = [
     '卂', '乃', '匚', '刀', '乇', '下', '厶', '卄', '工', '丁', '长', '乚', '从', '𠘨', '口',
     '尸', '㔿', '尺', '丂', '丅', '凵', 'リ', '山', '乂', '丫', '乙'
 ]
-
+buletfont = [
+     '🅐', '🅑', '🅒', '🅓', '🅔', '🅕', '🅖', '🅗', '🅘', '🅙', '🅚', '🅛', '🅜', '🅝', '🅞', '🅟', '🅠', '🅡', '🅢', '🅣', '🅤',
+     '🅥', '🅦', '🅧', '🅨', '🅩'
+]
 
 def weebify(update: Update, context: CallbackContext):
     args = context.args
@@ -38,7 +41,35 @@ def weebify(update: Update, context: CallbackContext):
     else:
         message.reply_text(string)
 
+def textbulet(update: Update, context: CallbackContext):
+    args = context.args
+    message = update.effective_message
+    string = ""
+
+    if message.reply_to_message:
+        string = message.reply_to_message.text.lower().replace(" ", "  ")
+
+    if args:
+        string = '  '.join(args).lower()
+
+    if not string:
+        message.reply_text(
+            "Usage is `/textbulet <text>`", parse_mode=ParseMode.MARKDOWN)
+        return
+
+    for normiecharacter in string:
+        if normiecharacter in normiefont:
+            buletcharacter = buletfont[normiefont.index(normiecharacter)]
+            string = string.replace(normiecharacter, buletcharacter)
+
+    if message.reply_to_message:
+        message.reply_to_message.reply_text(string)
+    else:
+        message.reply_text(string)
+
 
 WEEBIFY_HANDLER = CommandHandler("weebify", weebify)
+TEXTBULET_HANDLER = CommandHandler("textbulet", textbulet)
 
 dispatcher.add_handler(WEEBIFY_HANDLER)
+dispatcher.add_handler(TEXTBULET_HANDLER)

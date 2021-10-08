16c6077197bdb95e4fcd82cff33238fab4e2a8a8
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-10-08 19:30:30 +0700

    Update nhentai.py

--- bot/modules/nhentai.py
+++ bot/modules/nhentai.py
@@ -96,8 +96,8 @@ def nhentai_data(noombers):
     post = telegraph.create_page(
         f"{title}",
         html_content=post_content,
-        author_name="@TheEsseXBot", 
-        author_url="https://t.me/TheEsseXBot"
+        author_name="@MirroringV3Bot", 
+        author_url="https://t.me/MirroringV3Bot"
     )
     return title,tags,artist,total_pages,post['url'],links[0]
     

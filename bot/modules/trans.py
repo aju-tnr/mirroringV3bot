3b6602e589034ed9c7c3d72d30b2273e01e1f77c
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-10-08 19:52:13 +0700

    Update trans.py

--- bot/modules/trans.py
+++ bot/modules/trans.py
@@ -1,5 +1,5 @@
 from telegram import Message, Update, Bot, User
-from telegram.ext import Filters, MessageHandler, CommandHandler
+from telegram.ext import Filters, MessageHandler, CommandHandler, CallbackContext
 
 from requests import get
 
@@ -8,7 +8,7 @@ from bot import dispatcher
 base_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 api_key = 'trnsl.1.1.20180603T023816Z.763b39e3388b46d6.aa9abf45baceb438c96bb1593ce58199cc66c4f1'
 
-def translate(bot: Bot, update: Update):
+def translate(bot: Bot, update: Update, context: CallbackContext):
   message = update.effective_message
   text = message.reply_to_message.text
   translation = get(f'{base_url}?key={api_key}&text={text}&lang=en').json()

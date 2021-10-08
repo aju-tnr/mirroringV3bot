0ab9a6952814a1a2aadd239056614d48c06d3e36
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-10-08 13:41:36 +0700

    Add files via upload

---
+++ bot/modules/sitesearch.py
@@ -0,0 +1,93 @@
+import json
+import html
+import bs4
+import requests
+from bot import dispatcher
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update)
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    CallbackQueryHandler)
+
+info_btn = "More Information"
+kaizoku_btn = "Otakudesu ☠️"
+kayo_btn = "Kayo 🏴‍☠️"
+ganime_btn = "Ganime ☠️"
+prequel_btn = "⬅️ Prequel"
+sequel_btn = "Sequel ➡️"
+close_btn = "Close ❌"
+
+
+def site_search(update: Update, context: CallbackContext, site: str):
+    message = update.effective_message
+    args = message.text.strip().split(" ", 1)
+    more_results = True
+
+    try:
+        search_query = args[1]
+    except IndexError:
+        message.reply_text("Give something to search")
+        return
+
+    if site == "otakudesu":
+        search_url = f"https://otakudesu.moe/?s={search_query}"
+        html_text = requests.get(search_url).text
+        soup = bs4.BeautifulSoup(html_text, "html.parser")
+        search_result = soup.find_all("h2", {'class': "post-title"})
+
+        if search_result:
+            result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>Otakudesu</code>: \n"
+            for entry in search_result:
+                post_link = "https://otakudesu.moe/" + entry.a['href']
+                post_name = html.escape(entry.text)
+                result += f"• <a href='{post_link}'>{post_name}</a>\n"
+        else:
+            more_results = False
+            result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>Otakudesu</code>"
+
+    elif site == "kayo":
+        search_url = f"https://animekayo.com/?s={search_query}"
+        html_text = requests.get(search_url).text
+        soup = bs4.BeautifulSoup(html_text, "html.parser")
+        search_result = soup.find_all("h2", {'class': "title"})
+
+        result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>AnimeKayo</code>: \n"
+        for entry in search_result:
+
+            if entry.text.strip() == "Nothing Found":
+                result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>AnimeKayo</code>"
+                more_results = False
+                break
+
+            post_link = entry.a['href']
+            post_name = html.escape(entry.text.strip())
+            result += f"• <a href='{post_link}'>{post_name}</a>\n"
+          
+
+    buttons = [[InlineKeyboardButton("See all results", url=search_url)]]
+
+    if more_results:
+        message.reply_text(
+            result,
+            parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(buttons),
+            disable_web_page_preview=True)
+    else:
+        message.reply_text(
+            result, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+
+
+def kaizoku(update: Update, context: CallbackContext):
+    site_search(update, context, "otakudesu")
+
+
+def kayo(update: Update, context: CallbackContext):
+    site_search(update, context, "kayo")
+    
+    
+    
+OTAKUDESU_SEARCH_HANDLER = CommandHandler("otakudesu", otakudesu)
+KAYO_SEARCH_HANDLER = CommandHandler("kayo", kayo)
+
+dispatcher.add_handler(OTAKUDESU_SEARCH_HANDLER)
+dispatcher.add_handler(KAYO_SEARCH_HANDLER)
